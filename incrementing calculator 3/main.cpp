11c97d1ad0b679725dab4112162ee05f7a2d68ca
Aaron Covrig <aaron@Dominion.local>
2015-03-07 17:02:38 -0500

    Initial Commit

---
+++ incrementing calculator 3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  incrementing calculator 3
+//
+//  Created by Aaron Covrig on 3/7/15.
+//  Copyright (c) 2015 Aaron Covrig. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
