896fcbda7ae80ae259418544a44f313a4e9bca8d
Aaron Covrig <aaron@Dominion.local>
2015-03-21 11:56:58 -0400

    Incrementing calculator in C++; should be functional It does require the equation to be entered twice though...

--- incrementing calculator 3/main.cpp
+++ incrementing calculator 3/main.cpp
@@ -7,9 +7,28 @@
 //
 
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double i = 0.00001; // the amount to increment by
+double e = 0.0001; // the margin of error
+double y = 10; // the desired value
+double x = 0; // starting value of x
+double f = 2 * x * x * x * x; // seems like this has to be entered twice, once here and a second time inside the while statement...
+void iterate() {
+    while (abs(y - f) >= e) {
+        cout << "x = " << x << " y = " << f << endl;
+        x = x + i;
+        f = 2 * x * x * x * x; // here is the second spot for the function
+        }
+    if (abs(y - f) <= e) {
+        cout << "Success! An independant variable value of: " << x << " has a corresponding dependent variable value of: " << y << " Which is a solution" << endl;
+    }
+    
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    iterate();
     return 0;
 }
